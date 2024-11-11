ed3394dc5ac7a7af35c4a8c65cd7c414be811c89
Doyeon <ansehdus7@gmail.com>
2024-11-11 23:50:36 +0900

    feat: 헤더 알림추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,3 @@
-// src/components/Header.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
@@ -31,8 +30,25 @@ const Header: React.FC = () => {
 
         {/* 로그인/로그아웃 텍스트 및 드롭다운 */}
         <div className="flex items-center space-x-6">
+          {/* 알람 아이콘 추가 */}
+          <button className="relative text-gray-600 hover:text-blue-600 transition">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              className="bi bi-bell"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 16a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM3 11V6a5 5 0 0 1 10 0v5h1a1 1 0 0 1 1 1v1H2v-1a1 1 0 0 1 1-1h1ZM4 6a4 4 0 1 0 8 0v5H4V6Z"/>
+            </svg>
+            {/* 알람 배지 (예: 새로운 알람이 있을 때 표시) */}
+            <span className="absolute top-0 right-0 inline-block w-2.5 h-2.5 bg-red-500 rounded-full"></span>
+          </button>
+
+          {/* 로그인 상태에 따라 드롭다운 또는 로그인 버튼 표시 */}
           {isLoggedIn ? (
-            <Dropdown/> // 드롭다운 컴포넌트 사용
+            <Dropdown /> // 드롭다운 컴포넌트 사용
           ) : (
             <Link
               to="/login"
